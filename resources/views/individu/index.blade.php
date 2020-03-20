a7cdb21cb23e2e2333f65c59982c2e0161bc8258
= <=>
2020-03-20 09:49:42 +0000

    Bootstrap ajouté au projet

--- resources/views/individu/index.blade.php
+++ resources/views/individu/index.blade.php
@@ -1,27 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Index individus</title>
-</head>
-<body>
-    @foreach ($individus as $key => $individu)
-        <tr>
-            <td>
-                {{ $individu->id }}
-            </td>
-            <td>
-                {{ $individu->nom }}
-            </td>
-            <td>
-                {{ $individu->prenom }}
-            </td>
-            <td>
-                {{ $individu->email }}
-            </td>
-        </tr>
-    @endforeach
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+            <h1 class="display-3">Liste des individus</h1>
+            <table class="table table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nom</th>
+                    <th scope="col">Prenom</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Statut</th>
+                  </tr>
+                </thead>
+                <tbody>
+                @foreach ($individus as $key => $individu)
+                  <tr>
+                    <th scope="row">{{ $loop->iteration }}</th>
+                    <td>{{ $individu->nom }}</td>
+                    <td>{{ $individu->prenom }}</td>
+                    <td>{{ $individu->email }}</td>
+                    <td>{{ $individu->statut->libelle }}</td>
+                  </tr>
+                @endforeach
+                </tbody>
+              </table>
+@endsection
